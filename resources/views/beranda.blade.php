832b4bee7f1a3bb7eb296803b4884f8ab16041ba
yogtan <johanesyogtan305@gmail.com>
2023-07-27 11:08:09 +0700

    Navbar & Hero Page

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+
+
+    <section class="hero align-items-center">
+        <div class="container">
+           <h3>Gereja Paroki Meliau</h3>
+           <h1 style="color: orange">SELAMAT DATANG</h1>
+        </div>
+    </section>
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-lg ">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/LogoParoki.svg" width="80px" alt="logoParoki"> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Tentang Gereja
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Sejarah Gereja</a></li>
+                        <li><a class="dropdown-item" href="#">Pastor Paroki</a></li>
+                        <li><a class="dropdown-item" href="#">Dewan Paroki</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Sekretariat
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Formulir</a></li>
+
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Blog & berita
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Lingkungan Gereja</a></li>
+                        <li><a class="dropdown-item" href="#">Orang Muda Katolik</a></li>
+                        <li><a class="dropdown-item" href="#">Puta & Putri Altar</a></li>
+
+                    </ul>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
